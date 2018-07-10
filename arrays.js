88ffd88ef975fb979813cf61e04092f9db769f65
Erin Green <erin.y.green@gmail.com>
2018-07-10 21:28:56 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,59 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+
+
+
+
+function addElementToBeginningOfArray() {
+  var dogs = ["Cedar", "goose"];
+  dogs = ["Brindle", ...dogs];
+  return dogs; 
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  chocolateBars.unshift("Milky Way");
+  return chocolateBars
+} 
+
+function addElementToEndOfArray() {
+  var colors = ["Purple", "Gold"];
+  colors = [...colors, "Green"];
+  return colors; 
+}
+
+function destructivelyAddElementToEndOfArray () {
+  var cities = ["New York", "Chicago"];
+  cities.push("New Orleans");
+  return cities;
+}
+
+function accessElementInArray() {
+  var myArray = [1, 2, 3];
+  return myArray[1];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+  var myArrayTwo = [1, 2, 3];
+  myArrayTwo.shift();
+  return myArrayTwo;
+}
+
+function removeElementFromBeginningOfArray() {
+  var myArrayThree = [1, 2, 3];
+  myArrayThree = myArrayThree.slice(1);
+  return myArrayThree;
+}
+
+function destructivelyRemoveElementFromEndOfArray() {
+  var soccer = [1, 2, 3];
+  soccer.pop();
+  return soccer; 
+}
+
+function removeElementFromEndOfArray() {
+  var myArrayFour = [1, 2, 3];
+  myArrayFour.slice(0, myArrayFour.length-1);
+  return myArrayFour;
+}
