f7860c67f5af452bc4737166851edd0ba3951868
Erin Green <erin.y.green@gmail.com>
2018-07-10 21:29:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,7 @@
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-
-
-
-
-
-function addElementToBeginningOfArray() {
+function addElementToBeginningOfArray(array, element) {
   var dogs = ["Cedar", "goose"];
   dogs = ["Brindle", ...dogs];
   return dogs; 
