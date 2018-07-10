bf6175eaca6d3612d204e43c3421cc854b943ab5
Erin Green <erin.y.green@gmail.com>
2018-07-10 21:31:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array;
 } 
 
-function addElementToEndOfArray() {
+function addElementToEndOfArray(array, element) {
   var colors = ["Purple", "Gold"];
   colors = [...colors, "Green"];
   return colors; 
