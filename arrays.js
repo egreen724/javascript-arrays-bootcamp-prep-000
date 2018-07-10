e5988d04dee3d9117478d4475ecba240ff7394d4
Erin Green <erin.y.green@gmail.com>
2018-07-10 21:35:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyAddElementToEndOfArray (array, element) {
 }
 
 function accessElementInArray(array, index) {
-  return array[1];
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray() {
